f39d16d4a1caaaef23ca9e7938b6b65285419a0b
ptejas26 <ptejas.patelia@gmail.com>
2023-01-14 16:48:27 -0500

    Initial Commit

---
+++ WatchAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchAppDemo
+//
+//  Created by Apple on 14/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
